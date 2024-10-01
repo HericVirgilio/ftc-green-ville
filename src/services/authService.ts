cc159ff9f92b64689acea911739acb0a86c5f76a
Lucas Vaz <ti@avatim.com.br>
2024-10-01 18:44:36 -0300

    ajuste ip

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const login = async (email: string, password: string) => {
-  const response = await axios.post('http://localhost:5000/login', {
+  const response = await axios.post('http://26.10.62.10:5000/login', {
     username: email,
     password: password,
   });
